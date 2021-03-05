a9bb2f39ea1e373f3cfa71e80484720a6a76d6bd
Artem <artem-karganyan@bk.ru>
2021-03-05 21:36:10 +0300

    rfd

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ wsSerever.on('close', () => {
 
 
 //server listen port
-const PORT = process.env.SERVER_PORT ?? 3020;
+const PORT = process.env.SERVER_PORT ?? 3000;
 
 server.listen(PORT, () => {
   console.log(`Server has been started on port ${PORT}`)
